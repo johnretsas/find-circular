e9be2d28f02544b2f7a5d12b6a969f91f28fca2a
John Retsas <35172535+johnretsas@users.noreply.github.com>
2026-06-27 11:59:08 +0100

    fix: configuration and refactor

--- src/find-circular/index.ts
+++ src/find-circular/index.ts
@@ -1,40 +1,70 @@
+type Configuration = {
+  replaceToken?: string;
+};
+
 export function findCircular(
   obj: unknown,
   seen = new WeakSet(),
   path = new Set<object>(),
+  configuration: Configuration = { replaceToken: "[Circular]" },
 ): unknown {
   if (obj === null || typeof obj !== "object") {
     return obj;
   }
 
   if (path.has(obj)) {
-    return "[Circular]";
+    return configuration.replaceToken;
   }
 
+  const result = (Array.isArray(obj) ? [] : {}) as Record<
+    string | symbol,
+    unknown
+  >;
+
   if (seen.has(obj)) {
-    const result = (Array.isArray(obj) ? [] : {}) as Record<string | symbol, unknown>;
     path.add(obj);
+
     for (const key of [
       ...Object.keys(obj),
       ...Object.getOwnPropertySymbols(obj),
     ]) {
-      result[key] = findCircular((obj as Record<string | symbol, unknown>)[key], new WeakSet(), path);
+      result[key] = findCircular(
+        (obj as Record<string | symbol, unknown>)[key],
+        new WeakSet(),
+        path,
+      );
     }
+
     path.delete(obj);
+
+    return result;
+  } else {
+    seen.add(obj);
+    path.add(obj);
+
+    for (const key of [
+      ...Object.keys(obj),
+      ...Object.getOwnPropertySymbols(obj),
+    ]) {
+      result[key] = findCircular(
+        (obj as Record<string | symbol, unknown>)[key],
+        seen,
+        path,
+      );
+    }
+
+    path.delete(obj);
+
     return result;
   }
-
-  seen.add(obj);
-
-  const result = (Array.isArray(obj) ? [] : {}) as Record<string | symbol, unknown>;
-  path.add(obj);
-  for (const key of [
-    ...Object.keys(obj),
-    ...Object.getOwnPropertySymbols(obj),
-  ]) {
-    result[key] = findCircular((obj as Record<string | symbol, unknown>)[key], seen, path);
-  }
-  path.delete(obj);
-
-  return result;
 }
+
+const testObj = {
+  name: "John",
+  email: "superEmail",
+  address: { number: 10, street: "Armouries Way" },
+} as any;
+
+testObj.person = testObj;
+
+console.log(findCircular(testObj));
